6c92e615936440d29f9febe6a883e5fef4585c6a
Largehawiian <jeremoyorama@gmail.com>
2022-08-18 16:21:57 -0400

    Format Changes

--- classes/class_vmware.ps1
+++ classes/class_vmware.ps1
@@ -2,26 +2,26 @@ class vmware {
     [string]$Name
     [string]$NumCPU
     [String]$MemoryGB
-    [String]$Guest
-    [String]$PowerState
     [int]$ProvisionedSpaceGB
     [int]$UsedSpaceGB
+    [String]$Guest
+    [String]$PowerState
     [String]$VMHost
 
     vmare () {}
 
-    vmware ($Name, $NumCPU, $MemoryGB, $Guest, $PowerState, $ProvisionedSpaceGB, $UsedSpaceGB, $VMHost){
+    vmware ($Name, $NumCPU, $MemoryGB, $ProvisionedSpaceGB, $UsedSpaceGB, $Guest, $PowerState, $VMHost) {
         $this.name = $Name
         $this.NumCPU = $NumCPU
         $this.MemoryGB = $MemoryGB
-        $this.Guest = $Guest
-        $this.PowerState = $PowerState
         $this.ProvisionedSpaceGB = $ProvisionedSpaceGB
         $this.UsedSpaceGB = $UsedSpaceGB
+        $this.Guest = $Guest
+        $this.PowerState = $PowerState
         $this.VMhost = $VMHost
     }
 
-    static [vmware]VMReport ($InputObject){
-        return [vmware]::new($InputObject.Name, $InputObject.NumCPU, $InputObject.MemoryGB, $InputObject.Guest, $InputObject.PowerState, $InputObject.ProvisionedSpaceGB, $InputObject.UsedSpaceGB, $InputObject.VMhost.Name)
+    static [vmware]VMReport ($InputObject) {
+        return [vmware]::new($InputObject.Name, $InputObject.NumCPU, $InputObject.MemoryGB, $InputObject.ProvisionedSpaceGB, $InputObject.UsedSpaceGB, $InputObject.Guest, $InputObject.PowerState, $InputObject.VMhost.Name)
     }
 }
